91cebbd39df2558e350a8fc6b002b24729c2e8b0
@W2_Jayesh_69981 <jayeshbaraskar37@gmail.com>
2023-10-10 13:34:40 +0530

    Login with Registration UI Complete

--- app/src/main/java/com/example/Jayesh/firebaselogin/Activites/LoginActivity.java
+++ app/src/main/java/com/example/Jayesh/firebaselogin/Activites/LoginActivity.java
@@ -2,15 +2,25 @@ package com.example.Jayesh.firebaselogin.Activites;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.example.Jayesh.firebaselogin.R;
 
 public class LoginActivity extends AppCompatActivity {
-
+private TextView textViewSignUp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        textViewSignUp = findViewById(R.id.TextViewSignUp);
+        textViewSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, RegistrationActivity.class));
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/Jayesh/firebaselogin/Activites/RegistrationActivity.java
@@ -0,0 +1,16 @@
+package com.example.Jayesh.firebaselogin.Activites;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.Jayesh.firebaselogin.R;
+
+public class RegistrationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+    }
+}
